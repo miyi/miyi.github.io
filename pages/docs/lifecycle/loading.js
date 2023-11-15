eb862f36f1c84117361e672a5dcf70be4c0fcdd7
miyi <mylegojesus@gmail.com>
2023-11-14 21:20:00 -0800

    doc_table complete

--- pages/docs/lifecycle/loading.js
+++ pages/docs/lifecycle/loading.js
@@ -13,8 +13,8 @@ export const headings = {
 
 export const rulesTable = {
   headers: [
-    { label: "value", name: "accepted values", style: ["width: 30%"] },
-    { label: "example", name: "examples", style: ["width: 70%"] },
+    { label: "value", name: "accepted values", style: ["width: 33%"] },
+    { label: "example", style: ["width: 67%"] },
   ],
   rows: [
     {
@@ -23,7 +23,34 @@ export const rulesTable = {
     },
     {
       value: "function resolves to plain object",
-      example: '+loading="fetchItem()"',
+      example: [
+        '+loading="fetchItem()"',
+        'export const fetchItems = () => ({id: 15, name: "frame clamp" })',
+      ],
+    },
+  ],
+};
+
+export const decoratorsTable = {
+  headers: [
+    { label: "name", style: ["width: 33%"] },
+    { label: "purpose", style: ["width: 67%"] },
+  ],
+  rows: [
+    {
+      name: "+loading#root",
+      purpose:
+        "The scope initiated with #root inherits directly from the root scope.",
+    },
+    {
+      name: "+loading#plain",
+      purpose:
+        "When the state variables initiated in +loading are all non-dynamic, #plain will help improve performance.",
+    },
+    {
+      name: "+loading#init",
+      purpose:
+        "Specifically designed to work with modules, #init will initiate all preset state values of that module.",
     },
   ],
 };
@@ -205,5 +232,57 @@ export const hashPlain = {
   <br>
   <button +click="year++">increment year value</button>
 </div>
+  `,
+};
+
+export const usingHashInit = {
+  type: "html",
+  label: "index.html",
+  code: `
+<script type="dagger/modules">
+  {
+    name_tag: "./nameTag.html"
+  }
+</script>
+<body>
+  <name_tag +loading#init={}></name_tag>
+</body>
+  `,
+};
+
+export const settingHashInit = {
+  type: "html",
+  label: "nameTag.html",
+  code: `
+<script type="dagger/modules">
+  {
+    view: {uri: "#view", init: {
+      name: 'Jason',
+      age: 19,
+      role: 'student'
+    }}
+  }
+</script>
+<template id="view">
+  <div class="basic-demo-card">
+    <div>Hi, my name is \${name}!</div>
+    <div> I am \${age} years old.</div>
+    <div> I am currently a \${role}</div>
+  <div>
+<template>
+  `,
+};
+
+export const hashInitBrowser = {
+  html: `
+<div class="basic-demo-card" +loading="{
+  name: 'Jason',
+  age: 19,
+  role: 'student'
+}">
+  <div class="narration">Hi, my name is <span>\${name}.</span></div>
+  <div class="narration">I am <span>\${age}</span> years old.</div>
+  <div class="narration">I am currently a <span>\${role}</span>.</div>
+<div> 
   `,
 };
