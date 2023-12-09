aa7d7bf56f6360cc4e6c7b792683a700724f18cc
miyi <mylegojesus@gmail.com>
2023-12-08 16:25:48 -0800

    fixing demo_code

--- pages/docs/control/exist.js
+++ pages/docs/control/exist.js
@@ -9,7 +9,7 @@ export const rulesTable = {
       example: [`$exist="myBoolean"`],
     },
     {
-      value: "single javascript statement",
+      value: "single javascript expression",
       example: [
         `$exist="true"`,
         `$exist="myFunction(myScopeVariable)"`,
@@ -87,33 +87,75 @@ export const basicExample_scope = {
   `,
 };
 
-export const basicExample_function_html = {
+export const basicExample_expression_html = {
   type: "html",
   label: "html",
   code: `
-<div +loading>
-  <div $exist="false">
-    This element isn't render in the DOM 
-    because $exist is evaluated to be false.
+<div +loading="{
+  filter: null,
+  item1: { size: 'small', thingy: 'duck'},
+  item2: { size: 'big', thingy: 'truck'}
+}">
+
+  <div +loading="{...item1}" 
+    $exist="checkSize(item1, filter)">
+    I'm a \${size} \${thingy}.
   </div>
-  <div $exist="true">
-    Elements renders on the DOM because $exist
-    is evaluated to be true.
+  <div +loading="{...item2}" 
+    $exist="checkSize(item2, filter)">
+    I'm a \${size} \${thingy}.
   </div>
+
+  <button +click="filter='big'">
+    big size only</button>
+  <button +click="filter=null">
+    clear size filter
+  </button>
 </div>
   `,
 };
 
-export const basicExample_function_browser = {
+export const basicExample_expression_js = {
+  type: "js",
+  label: "js",
+  code: `
+export const checkSize = (item, filter) => {
+  if (filter) {
+    return item.size == filter;
+  }
+  return true;
+};
+  `
+}
+
+export const basicExample_expression_browser = {
   html: `
 <div class="basic-demo-card" +loading="{
-  color: 'blue',
-  cards: ['red', 'blue', 'olive']
+  filter: null,
+  card1: {
+    size: 'small',
+    thingy: 'duck'
+  },
+  card2: {
+    size: 'big',
+    thingy: 'truck'
+  }
 }">
-  <div class="purple" $each="cards" $style="css" +loading="{css: 'background-color:' + item}" $exist="color==item">
-    The background color of this element is painted \${item} via the $style directive.
+  <div class="purple" +loading="{...card1}" $exist="checkSize(card1, filter)">
+    I'm a \${size} \${thingy}.
   </div>
-  <button +loading="{num: 0}" +click="color=cards[num%3], num++">change to \${cards[(num)%3]}</button>
+  <div class="purple" +loading="{...card2}" $exist="checkSize(card2, filter)">
+    I'm a \${size} \${thingy}.
+  </div>
+  <button +click="filter='big'">big size only</button>
+  <button +click="filter=null">clear size filter</button>
 </div>
   `,
+};
+
+export const checkSize = (item, filter) => {
+  if (filter) {
+    return item.size == filter;
+  }
+  return true;
 };
